6c6ebe363fbe921381453e6575faa05347e4e68c
Dmitriifd <idimka9@gmail.com>
2023-07-19 21:36:07 +0500

    users page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import DashLayout from 'components/DashLayout';
 import Login from 'features/auth/Login';
 import Welcome from 'features/auth/Welcome';
 import NotesList from 'features/notes/NotesList';
+import UsersList from 'features/users/UsersList';
 
 function App() {
   return (
@@ -15,8 +16,13 @@ function App() {
 
         <Route path='dash' element={<DashLayout />}>
           <Route index element={<Welcome />} />
+
           <Route path='notes'>
             <Route index element={<NotesList />} />
+          </Route>
+
+          <Route path='users'>
+            <Route index element={<UsersList />} />
           </Route>
         </Route>
       </Route>
